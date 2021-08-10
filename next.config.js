2f4fd4da80e40d622aa3d7f709591b9ff495cd3c
Peter Ahlgren <ahlgren1234@gmail.com>
2021-08-10 10:38:23 +0200

    Added map for search results.

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
         domains: ['links.papareact.com']
     },
+    env: {
+        mapbox_key: 'pk.eyJ1IjoiYWhsZ3JlbjEyMzQiLCJhIjoiY2tzNXJndGQ2MDR6eDJvczBnZnV4YnJvdyJ9.8HO5kJk2UZzQENPjZkdlkg'
+    }
 }
